e6976e7a66f400b12eda17f92d2df1078d2ec5e4
gkmit-dikshant <dikshant@gkmit.co>
2026-01-19 13:00:32 +0530

    feat: add default user seeder

--- notification-api/seeders/20260116090355-default-users.js
+++ notification-api/seeders/20260116090355-default-users.js
@@ -1,22 +1,51 @@
 "use strict";
-
+const fs = require("fs").promises;
+const path = require("path");
 const defaultPassword = require("../helpers/defaultPassword.helper");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    const defaultUsername = ["gkmit", "accel"];
+    const targetSchema = "public";
+    const tableName = {
+      tableName: "users",
+      schema: targetSchema,
+    };
+    try {
+      const clientDetailFilePath = path.join(
+        process.cwd(),
+        "..",
+        "clientList.json"
+      );
 
-    const defaultUsers = await Promise.all(
-      defaultUsername.map(async (user) => {
-        return {
-          username: user,
-          password: await defaultPassword(user),
-        };
-      })
-    );
+      const clients = JSON.parse(
+        await fs.readFile(clientDetailFilePath, "utf-8")
+      );
 
-    await queryInterface.bulkInsert("users", defaultUsers);
+      if (!Array.isArray(clients) || clients.length === 0) {
+        throw new Error("No client found!");
+      }
+
+      const defaultUsername = clients.map((client) => client.ID.toLowerCase());
+
+      if (!defaultUsername || defaultUsername.length === 0) {
+        throw new Error("No client found");
+      }
+
+      const defaultUsers = await Promise.all(
+        defaultUsername.map(async (user) => {
+          return {
+            username: `${user}@admin`,
+            password: await defaultPassword(user),
+          };
+        })
+      );
+
+      await queryInterface.bulkInsert(tableName, defaultUsers);
+    } catch (error) {
+      console.error("Seeder failed:", error);
+      throw error;
+    }
   },
 
   async down(queryInterface, Sequelize) {
