194e0d933d5c084a97cb2bdbc2fbb794ccb6f104
Vidit Khandelwal <vidit@gkmit.co>
2026-01-30 12:11:20 +0530

    fix: pagination details for logs api

--- notification-api/src/logs-api/controller.js
+++ notification-api/src/logs-api/controller.js
@@ -46,8 +46,8 @@ const messageLogs = async (req, res) => {
       message: "Data fetched successfully",
       data,
       pagination: {
-        page,
-        limit,
+        page: +page,
+        limit: data.length,
         totalPages,
       },
     });
