5c0b4723f59055213524916e2d094634e5c7eec7
Dikshant Sharma <dikshant@gkmit.co>
2026-02-17 12:05:12 +0530

    fix: custom from email handling (#53)
    * feat: add allowCustomFromEmail handling for email
    
    * fix: update login validation to skip regex

--- notification-api/src/notify/service.js
+++ notification-api/src/notify/service.js
@@ -6,6 +6,111 @@ const { loadClientConfigs } = require("../utillity/loadClientConfigs");
 
 let configs = null;
 
+/**
+ * Helpers
+ */
+
+const getClientConfig = async (clientId) => {
+  configs = configs || (await loadClientConfigs());
+  return configs?.find((conf) => conf.ID === clientId);
+};
+
+function getDefaultProvider(config, service) {
+  const serviceObj = config?.[service?.toUpperCase()];
+  if (!serviceObj) return null;
+
+  for (const [provider, providerConfig] of Object.entries(serviceObj)) {
+    if (typeof providerConfig === "object" && providerConfig?.default) {
+      return provider;
+    }
+  }
+  return null;
+}
+
+/**
+ * Service Guards
+ */
+
+const serviceEnforcers = {
+  EMAIL: ({ provider, message, clientConfig }) => {
+    if (!provider) return;
+
+    const { service, content, clientId } = message;
+
+    const serviceConfig =
+      clientConfig[service.toUpperCase()][provider.toUpperCase()];
+
+    const hasFromEmail = Boolean(content?.fromEmail?.length);
+    if (!serviceConfig.allowCustomFromEmail && hasFromEmail) {
+      throw {
+        statusCode: 400,
+        message: `fromEmail is not allowed in ${service} for client ${clientId}`,
+      };
+    }
+
+    if (serviceConfig.allowCustomFromEmail && !hasFromEmail) {
+      throw {
+        statusCode: 400,
+        message: `fromEmail can't be empty`,
+      };
+    }
+
+    if (!serviceConfig.allowCustomFromEmail) {
+      content.fromEmail = serviceConfig.SENDER_EMAIL;
+    }
+  },
+
+  SMS: () => {},
+
+  SLACK: () => {},
+};
+
+const serviceGuard = (provider, message, clientConfig) => {
+  const service = message.service?.toUpperCase();
+  serviceEnforcers[service]?.({ provider, message, clientConfig });
+};
+
+/**
+ * Provider Selection
+ */
+
+const selectProvider = async (service, destination, clientId) => {
+  const clientConfig = await getClientConfig(clientId);
+  const defaultProvider = getDefaultProvider(clientConfig, service);
+  try {
+    if (service === "sms") {
+      const parsed = parsePhoneNumberFromString(destination);
+      const countryCode = parsed?.countryCallingCode;
+
+      const dbConnect = await global.connectionManager.getModels(clientId);
+
+      const routingRole = await dbConnect.RoutingRule.findOne({
+        where: {
+          service: service.toUpperCase(),
+          match_value: countryCode,
+        },
+      });
+
+      return routingRole?.provider || defaultProvider?.toUpperCase();
+    }
+
+    if (service === "email") {
+      return defaultProvider?.toUpperCase();
+    }
+
+    return defaultProvider?.toUpperCase();
+  } catch (error) {
+    throw {
+      statusCode: 400,
+      message: error.message,
+    };
+  }
+};
+
+/**
+ * Create Notification Records
+ */
+
 const creatingNotificationRecord = async (
   clientId,
   service,
@@ -13,12 +118,10 @@ const creatingNotificationRecord = async (
   content,
   templateId = null,
 ) => {
-  configs = configs ? configs : await loadClientConfigs();
+  const clientConfig = await getClientConfig(clientId);
+  const enabledServices = clientConfig?.ENABLED_SERVERICES;
 
-  const enabledServices = configs?.filter((conf) => conf.ID === clientId)[0]
-    ?.ENABLED_SERVERICES;
-
-  if (!enabledServices || !Array.isArray(enabledServices)) {
+  if (!Array.isArray(enabledServices)) {
     throw {
       statusCode: 500,
       message: `invalid or missing ENABLED_SERVERICES in client config for ${clientId}`,
@@ -31,10 +134,22 @@ const creatingNotificationRecord = async (
       message: `${service} is not enable for client ${clientId}`,
     };
   }
-  let dbConnect = await global.connectionManager.getModels(clientId);
-  const results = await Promise.all(
-    destination.map(async (number) => {
-      try {
+
+  const dbConnect = await global.connectionManager.getModels(clientId);
+
+  try {
+    const results = await Promise.all(
+      destination.map(async (number) => {
+        /**
+         * Service specific enforcement
+         */
+        const provider = await selectProvider(service, number, clientId);
+        serviceGuard(provider, { service, content, clientId }, clientConfig);
+        if (service.toLowerCase() === "slack") {
+          service = "slackbot";
+        }
+
+        content.provider = provider;
         const record = await dbConnect.Notification.create({
           messageId: uuidv4(),
           service,
@@ -50,70 +165,38 @@ const creatingNotificationRecord = async (
           ...record.dataValues,
         });
 
-        return {
-          success: true,
-          number,
-          ...record.dataValues,
-        };
-      } catch (dbError) {
-        logger.error("Database error: Failed to create notification record", {
-          number,
-          error: dbError.message,
-        });
+        return { success: true, number, ...record.dataValues };
+      }),
+    );
 
-        if (dbError.name === "SequelizeUniqueConstraintError") {
-          return {
-            success: false,
-            number,
-            statusCode: 409,
-            message:
-              "Conflict: A notification with this identifier potentially exists.",
-          };
-        }
-
-        return {
-          success: false,
-          number,
-          statusCode: 500,
-          message: "Failed to create notification",
-          error: dbError.message,
-        };
-      }
-    }),
-  );
-
-  return results;
-};
-
-const selectProvider = async (service, destination, clientId) => {
-  try {
-    if (service === "sms") {
-      const countryCode =
-        parsePhoneNumberFromString(destination).countryCallingCode;
-      let dbConnect = await global.connectionManager.getModels(clientId);
-      const routingRole = await dbConnect.RoutingRule.findOne({
-        where: {
-          service: service.toUpperCase(),
-          match_value: countryCode,
-        },
-      });
-
-      return routingRole?.provider;
-    } else if (service === "email") {
-      // TODO check for email routing
-      return;
-    }
+    return results;
   } catch (error) {
-    console.log(error);
-    return {
-      statusCode: 400,
-      message: error.message,
+    logger.error("Failed to create notification record", {
+      error: error.message,
+      error,
+    });
+
+    if (error.name === "SequelizeUniqueConstraintError") {
+      throw {
+        statusCode: 409,
+        message:
+          "Conflict: A notification with this identifier potentially exists.",
+      };
+    }
+
+    throw {
+      statusCode: error.statusCode || 500,
+      message: error.message || "Failed to create notification",
     };
   }
 };
 
+/**
+ * Publish Message
+ */
+
 const publishingNotificationRequest = async (notificationRecord) => {
-  let {
+  const {
     service,
     destination,
     content,
@@ -122,36 +205,33 @@ const publishingNotificationRequest = async (notificationRecord) => {
     fileId = undefined,
     attachments,
   } = notificationRecord;
-  const provider = await selectProvider(service, destination, clientId);
 
   const rabbitConnect = await rabbitManager.getClient(clientId);
 
-  if (service.toLowerCase() === "slack") {
-    service = "slackbot";
-  }
-  if (rabbitConnect) {
-    const result = await rabbitConnect.publishMessage(service, {
-      service,
-      destination,
-      content,
-      messageId,
-      clientId,
-      timestamp: new Date().toISOString(),
-      provider,
-      fileId,
-      attachments,
-    });
+  if (!rabbitConnect) return;
 
-    return result;
-  }
+  return rabbitConnect.publishMessage(service, {
+    service,
+    destination,
+    content,
+    messageId,
+    clientId,
+    timestamp: new Date().toISOString(),
+    provider: content?.provider,
+    fileId,
+    attachments,
+  });
 };
 
+/**
+ * Fetch Notification
+ */
+
 const getNotificationData = async (messageId, clientID) => {
-  let dbConnect = await global.connectionManager.getModels(clientID);
+  const dbConnect = await global.connectionManager.getModels(clientID);
+
   const details = await dbConnect.Notification.findOne({
-    where: {
-      messageId: messageId,
-    },
+    where: { messageId },
   });
 
   if (!details) {
@@ -169,12 +249,9 @@ const getNotificationData = async (messageId, clientID) => {
     attachments: details.content.attachments,
   };
 
-  if (details.content.cc) {
-    data.cc = details.content.cc;
-  }
-  if (details.content.bcc) {
-    data.bcc = details.content.bcc;
-  }
+  if (details.content.cc) data.cc = details.content.cc;
+  if (details.content.bcc) data.bcc = details.content.bcc;
+
   return data;
 };
 
