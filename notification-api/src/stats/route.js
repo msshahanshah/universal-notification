a91111807c23eb946b581888b972638dbfb90a3c
divyabagora-GKMIT <divyabagora@gkmit.co>
2026-02-19 15:41:23 +0530

    fix: change method of refresh balance api

--- notification-api/src/stats/route.js
+++ notification-api/src/stats/route.js
@@ -4,7 +4,7 @@ const auth = require("../middleware/auth.middleware");
 const accessControl = require("../middleware/access-control.middleware");
 const statRouter = express.Router();
 
-statRouter.get("/refresh-balance", auth, accessControl, getBalance);
+statRouter.post("/refresh-balance", auth, accessControl, getBalance);
 statRouter.get("/balance", auth, accessControl, viewBalance);
 
 module.exports = statRouter;
