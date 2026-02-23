49aa193a5465b3634dd1c8da07dde1eff2d7f9b3
gkmit-dikshant <dikshant@gkmit.co>
2026-02-23 14:00:13 +0530

    fix: add handling for redis client down

--- notification-api/src/utillity/redisClient.js
+++ notification-api/src/utillity/redisClient.js
@@ -2,10 +2,19 @@ const { createClient } = require("redis");
 
 const redisClient = createClient({
   url: process.env.REDIS_URL,
+  disableOfflineQueue: false,
+  socket: {
+    reconnectStrategy: (retries) => {
+      if (retries > 5) {
+        return new Error("Redis retry attempts exhausted");
+      }
+      return Math.min(retries * 100, 3000);
+    },
+  },
 });
 
 redisClient.on("error", (err) => {
-  console.error("Redis Client Error", err);
+  throw { statusCode: 500, message: "Redis Client Offline" };
 });
 
 (async () => {
