08922a90b00b9aebecaa74cf371f0c840155dfed
Vidit Khandelwal <vidit@gkmit.co>
2026-02-26 14:53:04 +0530

    feat: add twilio provider for message sending on whatsapp

---
+++ whatsapp-server/src/utility/whatsAppSender.js
@@ -0,0 +1,89 @@
+const twilio = require('twilio');
+const logger = require('../logger');
+
+class WhatsAppSender {
+  constructor(clientId, clientConfig) {
+    this.clientConfig = clientConfig;
+    this.provider = null;
+    this.sender = null;
+    this.providerInitializer = {
+      TWILIO: this.setupWhatsAppTwilio,
+    };
+    this.defaultProvider = Object.entries(this.clientConfig).find(
+      ([service, config]) => {
+        return config.default === true;
+      },
+    )[0];
+
+    if (!this.defaultProvider) {
+      throw new Error(`no default config for whatsapp for client ${clientId}`);
+    }
+  }
+
+  async initialize(provider = 'default') {
+    let providerKey = provider;
+
+    if (provider === 'default') {
+      providerKey = this.defaultProvider;
+    }
+
+    const initializer = this.providerInitializer[providerKey];
+
+    if (typeof initializer === 'function') {
+      await initializer.call(this);
+      return;
+    }
+
+    console.error(
+      'Invalid whatsapp configuration:',
+      JSON.stringify(this.clientConfig, null, 2),
+    );
+    throw new Error(`Unsupported whatsapp provider: ${provider}`);
+  }
+
+  async setupWhatsAppTwilio() {
+    const { ACCOUNT_SID, AUTH_TOKEN, FROM_NUMBER } = this.clientConfig.TWILIO;
+    const client = twilio(ACCOUNT_SID, AUTH_TOKEN);
+
+    this.sender = async ({fromNumber, templateId, attachment, message, destination}, messageId) => {
+      logger.info('Sending WhatsApp via Twilio...');
+
+      if(typeof attachment === 'object') {
+        attachment = attachment.url;
+      }
+
+      if(!fromNumber){
+         fromNumber = FROM_NUMBER;
+      }
+
+        const res = await client.messages.create({
+          from: `whatsapp:${fromNumber}`,
+          to: `whatsapp:${destination}`,
+          body: message,
+          mediaUrl: [attachment],
+          templateId: templateId
+        });
+      return res;
+    };
+
+    this.provider = 'TWILIO';
+  }
+
+  async sendWhatsAppMessage({fromNumber, templateId, attachment, message, destination}, messageId) {
+    if (!this.sender)
+      throw new Error('WhatsApp message sender not initialized');
+    try {
+      const result = await this.sender({fromNumber, templateId, attachment, message, destination}, messageId);
+      logger.info(`SMS sent via whatsapp, provider: ${this.provider}`);
+      return result;
+    } catch (err) {
+      logger.error(`Error in sending whatsapp message via ${this.provider}`);
+      console.log(err);
+      throw err;
+    }
+  }
+}
+
+module.exports = {
+  WhatsAppSender,
+};
