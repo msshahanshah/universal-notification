e2a47c06d3c5b5147a144ee6528d9d6529e1143d
divyabagora-GKMIT <divyabagora@gkmit.co>
2026-03-06 21:57:18 +0530

    fix: refresh token bug

--- notification-api/src/auth/service.js
+++ notification-api/src/auth/service.js
@@ -85,13 +85,17 @@ const generateNewAccessToken = async (refreshToken, x_clientId) => {
       throw { statusCode: 400, message: `invalid username or password` };
     }
     const REDIS_ACCESS_TOKEN_KEY = RedisHelper.getAccessTokenRedisKey(username);
-    const REDIS_REFRESH_TOKEN_KEY =
-      RedisHelper.getRefreshTokenRedisKey(username);
+    const REDIS_REFRESH_TOKEN_KEY = RedisHelper.getRefreshTokenRedisKey(username);
 
     const isRefreshTokenExistInRedis = await RedisHelper.getValue(
       REDIS_REFRESH_TOKEN_KEY,
     );
-    if (!isRefreshTokenExistInRedis) {
+
+    if (!isRefreshTokenExistInRedis ) {
+      throw { message: "Unauthorized", statusCode: 401 };
+    }
+
+    if (isRefreshTokenExistInRedis !== refreshToken){
       throw { message: "Unauthorized", statusCode: 401 };
     }
 
