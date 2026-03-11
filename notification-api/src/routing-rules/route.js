e38cf2784749cb62df52e887149bd726890c6491
divyabagora-GKMIT <divyabagora@gkmit.co>
2026-03-11 11:48:12 +0530

    feat: add delete api for routing rule

--- notification-api/src/routing-rules/route.js
+++ notification-api/src/routing-rules/route.js
@@ -2,9 +2,10 @@ const express = require("express");
 const routingRuleRouter = express();
 const auth = require("../middleware/auth.middleware");
 const accessControl = require("../middleware/access-control.middleware");
-const createRouting = require("./controller");
+const { createRouting, removeRouting } = require("./controller");
 const validateRequest = require("./validation")
 
 routingRuleRouter.post("/routing-rules", auth, accessControl, validateRequest, createRouting);
+routingRuleRouter.delete("/routing-rules/:ruleId", auth, accessControl, removeRouting)
 
 module.exports = routingRuleRouter;
