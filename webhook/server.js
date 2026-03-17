96098196968049e51b696f997545de161caf3381
Vidit Khandelwal <vidit@gkmit.co>
2026-03-17 11:50:58 +0530

    feat: add webhook server

--- webhook/server.js
+++ webhook/server.js
@@ -1,26 +1,30 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const fastify = require("fastify")({ logger: true });
-const mongoose = require("mongoose");
+const fastify = require('fastify')({ logger: true });
+const mongoose = require('mongoose');
 
-const webhookRoutes = require("./routes/webhookRoutes");
-const {connectMongoose} = require('./helpers/mongoose.helper')
+const webhookRoutes = require('./routes/webhookRoutes');
+const { connectMongoose } = require('./helpers/mongoose.helper');
+const { startGrpcServer } = require('./grpc/grpc.server');
+
+require('./cron/webhookCron');
 
 /**
  * ✅ Enable CORS
  */
-fastify.register(require("@fastify/cors"), {
+fastify.register(require('@fastify/cors'), {
   origin: true, // allow all origins (POC)
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
 });
 
-fastify.register(webhookRoutes, { prefix: "/api/webhook" });
+fastify.register(webhookRoutes, { prefix: '/api/webhook' });
 
 const start = async () => {
   try {
     await connectMongoose();
+    startGrpcServer();
     await fastify.listen({ port: 3000 });
-    console.log("Server running on port 3000");
+    console.log('Server running on port 3000');
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
